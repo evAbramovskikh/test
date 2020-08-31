ded4cc0bd05cc910b9a7741b1d1d1cac09c157ec
Evgenii Abramovskikh <ev.abramovskikh@gmail.com>
2020-08-31 15:23:25 +0700

    complete test

--- src/index.js
+++ src/index.js
@@ -1,1 +1,24 @@
 const css = require('./style.scss');
+
+document.querySelector('.combat-controls-attack').addEventListener('click', function () {
+    document.querySelector(".combat-controls-attack").classList.add("combat-controls-animation")
+    document.querySelector(".unit-sprite-enemy").classList.add("unit-sprite-enemy-animation")
+    document.querySelector(".unit-sprite-enemy-plume").classList.add("unit-sprite-enemy-plume-animation")
+    setTimeout(function(){
+        document.querySelector(".combat-controls-attack").classList.remove("combat-controls-animation")
+        document.querySelector(".unit-sprite-enemy").classList.remove("unit-sprite-enemy-animation")
+        document.querySelector(".unit-sprite-enemy-plume").classList.remove("unit-sprite-enemy-plume-animation")
+    }, 600)
+});
+
+document.querySelector('.combat-controls-skill').addEventListener('click', function () {
+    document.querySelector(".unit-sprite-enemy").classList.add("unit-sprite-enemy-skill-animation")
+    document.querySelector(".unit-sprite-enemy").classList.toggle("unit-sprite-enemy-skill")
+    document.querySelector(".combat-controls-skill").classList.add("combat-controls-skill-animation")
+    document.querySelector(".unit-sprite-enemy-plume").classList.add("unit-sprite-enemy-plume-skill-animation")
+    setTimeout(function(){
+        document.querySelector(".combat-controls-skill").classList.remove("combat-controls-skill-animation")
+        document.querySelector(".unit-sprite-enemy").classList.remove("unit-sprite-enemy-skill-animation")
+        document.querySelector(".unit-sprite-enemy-plume").classList.remove("unit-sprite-enemy-plume-skill-animation")
+    }, 1000)
+});
